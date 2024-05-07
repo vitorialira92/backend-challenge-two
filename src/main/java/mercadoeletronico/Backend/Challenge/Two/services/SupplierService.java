973638efddcaed5775bbabec13b1b50b39011ef9
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-07 15:56:44 -0300

    feat: get all and get by id

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/services/SupplierService.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/services/SupplierService.java
@@ -1,11 +1,23 @@
 package mercadoeletronico.Backend.Challenge.Two.services;
 
+import mercadoeletronico.Backend.Challenge.Two.domain.supplier.Supplier;
 import mercadoeletronico.Backend.Challenge.Two.repositories.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SupplierService {
     @Autowired
     private SupplierRepository repository;
+
+    public List<Supplier> getAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Supplier> getById(Long id) {
+        return repository.findById(id);
+    }
 }
