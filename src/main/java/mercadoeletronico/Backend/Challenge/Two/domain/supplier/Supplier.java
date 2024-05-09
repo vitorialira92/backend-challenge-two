89167308fb751c9eeb5df12f7deb3209fc9d8069
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-09 12:19:43 -0300

    fix: main contact was missing

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/domain/supplier/Supplier.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/domain/supplier/Supplier.java
@@ -9,10 +9,69 @@ import java.util.List;
 public class Supplier {
     @Id
     private String id;
-    private String name;
-    private SupplierType type;
-    private String document;
+    private final String name;
+    private final SupplierType type;
+    private final DocumentType documentType;
+    private final String document;
+    private SupplierMainContact mainContact;
     private List<String> phoneNumbers;
     private String activitiesDescription;
 
+    public Supplier(String name, SupplierType type, String document, SupplierMainContact mainContact, List<String> phoneNumbers, String activitiesDescription) {
+        this.name = name;
+        this.type = type;
+        this.document = document;
+        this.mainContact = mainContact;
+        this.phoneNumbers = phoneNumbers;
+        this.activitiesDescription = activitiesDescription;
+        this.documentType = this.type == SupplierType.INDIVIDUAL ? DocumentType.CPF : DocumentType.CNPJ;
+    }
+
+    public DocumentType getDocumentType() {
+        return documentType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SupplierType getType() {
+        return type;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public SupplierMainContact getMainContact() {
+        return mainContact;
+    }
+
+    public void setMainContact(SupplierMainContact mainContact) {
+        this.mainContact = mainContact;
+    }
+
+    public List<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(List<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public String getActivitiesDescription() {
+        return activitiesDescription;
+    }
+
+    public void setActivitiesDescription(String activitiesDescription) {
+        this.activitiesDescription = activitiesDescription;
+    }
 }
