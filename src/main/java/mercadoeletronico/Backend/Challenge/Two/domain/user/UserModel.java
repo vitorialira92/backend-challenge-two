42f6591b578be3df0f9352a83ef107ea47146b10
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-09 12:21:35 -0300

    feat: user model

---
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/domain/user/UserModel.java
@@ -0,0 +1,70 @@
+package mercadoeletronico.Backend.Challenge.Two.domain.user;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserModel implements UserDetails {
+    @Id
+    private String id;
+    private String email;
+    private String password;
+
+    public UserModel(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String email) {
+        this.email = email;
+    }
+}
