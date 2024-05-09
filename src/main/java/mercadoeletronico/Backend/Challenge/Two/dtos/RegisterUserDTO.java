94e4f2bc52793ab3122c293fea1f5b14cf9044fb
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-09 12:24:44 -0300

    feat: register user dto

---
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/dtos/RegisterUserDTO.java
@@ -0,0 +1,26 @@
+package mercadoeletronico.Backend.Challenge.Two.dtos;
+
+import jakarta.validation.constraints.NotNull;
+
+public class RegisterUserDTO {
+    @NotNull
+    public String email;
+    @NotNull
+    public String password;
+
+    public @NotNull String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotNull String password) {
+        this.password = password;
+    }
+
+    public @NotNull String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotNull String email) {
+        this.email = email;
+    }
+}
