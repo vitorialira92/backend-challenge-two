9d2ed010408558532645cc891990019540fd7a54
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-17 11:17:50 -0300

    feat: method to find all suppliers that a user has

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/repositories/SupplierRepository.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/repositories/SupplierRepository.java
@@ -2,9 +2,11 @@ package mercadoeletronico.Backend.Challenge.Two.repositories;
 
 import mercadoeletronico.Backend.Challenge.Two.domain.supplier.Supplier;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface SupplierRepository extends MongoRepository<Supplier, String> {
     Optional<Supplier> findByDocument(String document);
+    List<Supplier> findAllByUserId(String userId);
+
 }
