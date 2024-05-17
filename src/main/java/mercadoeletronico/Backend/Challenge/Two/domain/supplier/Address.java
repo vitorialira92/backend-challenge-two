278cbfda18e94a147a5a524ba1a9c50a15f1c91e
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-17 11:19:49 -0300

    feat: constructor

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/domain/supplier/Address.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/domain/supplier/Address.java
@@ -14,4 +14,15 @@ public class Address {
     private String neighborhood;
     private String city;
     private State state;
+
+    public Address(String zipcode, String street, String number,
+                   String complement, String neighborhood, String city, State state) {
+        this.zipcode = zipcode;
+        this.street = street;
+        this.number = number;
+        this.complement = complement;
+        this.neighborhood = neighborhood;
+        this.city = city;
+        this.state = state;
+    }
 }
