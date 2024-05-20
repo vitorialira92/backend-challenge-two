475bb2773002c690518f827259f77aaa1681b3e7
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-19 23:30:35 -0300

    fix: SupplierRepository should have @Repository annotation

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/repositories/SupplierRepository.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/repositories/SupplierRepository.java
@@ -2,9 +2,12 @@ package mercadoeletronico.Backend.Challenge.Two.repositories;
 
 import mercadoeletronico.Backend.Challenge.Two.domain.supplier.Supplier;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface SupplierRepository extends MongoRepository<Supplier, String> {
     Optional<Supplier> findByDocument(String document);
     List<Supplier> findAllByUserId(String userId);
