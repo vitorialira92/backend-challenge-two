d89cedc29ec35461f417a73c6cf1fa0208260fd8
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-19 23:30:23 -0300

    fix: UserRepository should have @Repository annotation

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/repositories/UserRepository.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package mercadoeletronico.Backend.Challenge.Two.repositories;
 
 import mercadoeletronico.Backend.Challenge.Two.domain.user.UserModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
     Optional<UserModel> findByEmail(String email);
 }
