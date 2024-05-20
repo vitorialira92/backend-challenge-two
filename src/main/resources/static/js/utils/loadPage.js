d9c7031d5b69fed839b05ab7f78d74cb24b934b7
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-19 23:27:25 -0300

    feat: loadPage function to send a request to load a page with auth token

---
+++ src/main/resources/static/js/utils/loadPage.js
@@ -0,0 +1,25 @@
+function loadPage(url) {
+    const token = localStorage.getItem('authToken');
+    $.ajax({
+        url: url,
+        type: 'GET',
+        headers: {
+            'Authorization': token ? 'Bearer ' + token : ''
+        },
+        success: function(response) {
+            $('body').html(response);
+        },
+        error: function(xhr, status, error) {
+            Swal.fire({
+                title: 'Não autorizado.',
+                text: 'Faça login novamente.',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            }).then((result)=>{
+                if (result.value) {
+                    window.location.href = '/auth/login';
+                }
+            });
+        }
+    });
+}
