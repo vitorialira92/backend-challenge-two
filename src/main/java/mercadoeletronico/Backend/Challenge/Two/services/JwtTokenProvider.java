5772d5dd671fce2f4551e84590535497918cd919
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-19 23:28:39 -0300

    feat: jwt token provider

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/services/JwtTokenProvider.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/services/JwtTokenProvider.java
@@ -4,27 +4,34 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import mercadoeletronico.Backend.Challenge.Two.domain.user.UserModel;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+@Component
 @Service
-public class TokenService {
-    @Value("${api:security:token:secret}")
+public class JwtTokenProvider {
+
+    @Value("${security:token:secret}")
     private String secret;
 
-    public String generateToken(User user){
+    //@Value("${security:token:expirationInMilliSeconds}")
+    private long expirationInHours = 240;
+
+    public String generateToken(UserModel user){
         try{
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            return JWT.create()
+            String token = JWT.create()
                     .withIssuer("auth-api")
                     .withSubject(user.getUsername())
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
+            return token;
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Error while generating token", exception);
         }
@@ -39,11 +46,12 @@ public class TokenService {
                     .verify(token)
                     .getSubject();
         } catch (JWTVerificationException exception){
+            System.out.println("não foi validado: " + exception.getMessage());
             return "";
         }
     }
 
     private Instant genExpirationDate(){
-        return LocalDateTime.now().plusHours(5).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusMinutes(expirationInHours).toInstant(ZoneOffset.of("-03:00"));
     }
 }
