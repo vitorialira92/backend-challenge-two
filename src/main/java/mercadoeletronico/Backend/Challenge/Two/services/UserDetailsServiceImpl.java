d58cf8c9da28926a2d87b4022a109c55ff5c9771
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-20 15:19:07 -0300

    feat: user details service

---
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package mercadoeletronico.Backend.Challenge.Two.services;
+
+import mercadoeletronico.Backend.Challenge.Two.domain.user.UserModel;
+import mercadoeletronico.Backend.Challenge.Two.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return user;
+    }
+
+    public UserDetails loadUserById(String userId) {
+        UserModel user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + userId));
+        return user;
+    }
+}
