efd94d3b637872ca3bc76ae84dc3007171335e4f
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-19 23:36:01 -0300

    feat: auth check common script

---
+++ src/main/resources/static/js/utils/auth-check.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    const userId = localStorage.getItem('userId');
+    if(!userId){
+        Swal.fire({
+            title: 'Erro!',
+            text: 'Usuário deslogado. Faça login novamente',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        }).then((result)=>{
+            if (result.value) {
+                window.location.href = '/auth/login';
+            }
+        });
+    }
+});
