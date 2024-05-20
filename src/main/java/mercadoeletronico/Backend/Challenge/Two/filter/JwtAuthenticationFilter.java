3a78b3262da5930d4e3e1ee3dccfb5bbc4ac0906
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-19 23:35:09 -0300

    feat: jwt authentication filter

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/filter/JwtAuthenticationFilter.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/filter/JwtAuthenticationFilter.java
@@ -1,28 +1,26 @@
 package mercadoeletronico.Backend.Challenge.Two.filter;
 
+import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import mercadoeletronico.Backend.Challenge.Two.domain.user.UserModel;
-import mercadoeletronico.Backend.Challenge.Two.repositories.UserRepository;
-import mercadoeletronico.Backend.Challenge.Two.services.TokenService;
+import mercadoeletronico.Backend.Challenge.Two.services.JwtTokenProvider;
+import mercadoeletronico.Backend.Challenge.Two.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Optional;
 
-@Component
-public class SecurityFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter implements Filter {
     @Autowired
-    TokenService tokenService;
+    JwtTokenProvider tokenService;
 
     @Autowired
-    UserRepository userRepository;
+    UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void doFilterInternal
@@ -31,23 +29,20 @@ public class SecurityFilter extends OncePerRequestFilter {
         var token = this.recoverToken(request);
 
         if (token != null) {
-            var email = tokenService.validateToken(token);
-            Optional<UserModel> user = userRepository.findByEmail(email);
+            var login = tokenService.validateToken(token);
+            UserDetails user = userDetailsService.loadUserByUsername(login);
 
-            if (user.isPresent()) {
-                var authentication = new UsernamePasswordAuthenticationToken(
-                        user.get(), null, user.get().getAuthorities());
+
+            if (user != null) {
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
         filterChain.doFilter(request, response);
     }
-
-
     private String recoverToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
         if (authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
     }
-
 }
