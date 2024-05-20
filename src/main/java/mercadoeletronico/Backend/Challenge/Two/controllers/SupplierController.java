346db0d5a84779798a28a568cdb3f7b529fccaef
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-19 23:26:01 -0300

    feat: full supplier controller implementation

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/controllers/SupplierController.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/controllers/SupplierController.java
@@ -1,13 +1,20 @@
 package mercadoeletronico.Backend.Challenge.Two.controllers;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+import mercadoeletronico.Backend.Challenge.Two.domain.supplier.Supplier;
+import mercadoeletronico.Backend.Challenge.Two.dtos.*;
+import mercadoeletronico.Backend.Challenge.Two.exceptions.DuplicateCreationAttemptException;
+import mercadoeletronico.Backend.Challenge.Two.exceptions.ResourceNotFoundException;
 import mercadoeletronico.Backend.Challenge.Two.services.SupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller()
 @RequestMapping("/supplier")
@@ -15,20 +22,54 @@ public class SupplierController {
     @Autowired
     private SupplierService service;
 
-    @GetMapping()
-    public String getAllSuppliers(Model model){
-        System.out.println("all");
-
-        model.addAttribute("suppliers", service.getAll());
-
-        return "all-suppliers";
+    @GetMapping("/all/{userId}")
+    @ResponseBody
+    public ResponseEntity<List<SupplierSimpleDTO>> getUsersAllSuppliers(@Parameter(description = "Supplier Id", example = "4848644589")
+                                                                            @PathVariable String userId) {
+        List<SupplierSimpleDTO> suppliersList = service.getAll(userId);
+        return ResponseEntity.ok(suppliersList);
     }
-
     @GetMapping("/{id}")
-    public String getSupplierById(@Parameter(description = "Supplier Id", example = "4848644589")
-                                      @PathVariable Long id, Model model){
-        System.out.println("by id");
-        model.addAttribute("supplier", service.getById(id));
-        return "supplier";
+    @ResponseBody
+    public ResponseEntity<SupplierDTO> getSupplierById(@Parameter(description = "Supplier Id", example = "4848644589")
+                                      @PathVariable String id, Model model){
+        try{
+            Optional<SupplierDTO> supplier = service.getById(id);
+            return supplier.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        }catch(ResourceNotFoundException ex){
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PostMapping("/{userId}")
+    public String createSupplier(@Parameter(description = "Supplier Id", example = "4848644589")
+                                     @PathVariable String userId, @ModelAttribute("creationDTO") @Valid SupplierCreationDTO supplierDTO){
+        try{
+            Supplier supplier = service.createSupplier(supplierDTO, userId);
+            return "redirect:/view-supplier/" + supplier.getId();
+        }catch(DuplicateCreationAttemptException ex){
+            return "error-message";
+        }
+    }
+    @PutMapping("/{id}")
+    public String updateSupplier(@Parameter(description = "Supplier Id", example = "4848644589")
+                                 @PathVariable String id,
+                                 @ModelAttribute("updateDTO") @Valid SupplierUpdateDTO updateDTO,
+                                 Model model){
+        try{
+            Optional<Supplier> supplier = service.updateSupplier(id, updateDTO);
+            model.addAttribute("supplier", supplier);
+            return "all-suppliers";
+        }catch(ResourceNotFoundException ex){
+            return "error-message";
+        }
+    }
+    @DeleteMapping("/{id}")
+    public String deleteSupplier(@PathVariable String id, Model model){
+        try{
+            service.deleteSupplier(id);
+            return "all-suppliers";
+        }catch(ResourceNotFoundException ex){
+            return "error-message";
+        }
     }
 }
