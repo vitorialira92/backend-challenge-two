49b3b6456a3ebacd2212c84b37a46eef38dab308
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-20 16:25:55 -0300

    refactor: authorization service now only does registration

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/services/AuthorizationService.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/services/AuthorizationService.java
@@ -1,28 +1,17 @@
 package mercadoeletronico.Backend.Challenge.Two.services;
 
 import mercadoeletronico.Backend.Challenge.Two.domain.user.UserModel;
-import mercadoeletronico.Backend.Challenge.Two.dtos.LoginDTO;
 import mercadoeletronico.Backend.Challenge.Two.dtos.RegisterUserDTO;
 import mercadoeletronico.Backend.Challenge.Two.exceptions.DuplicateCreationAttemptException;
 import mercadoeletronico.Backend.Challenge.Two.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorizationService {
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private TokenService tokenService;
 
     public UserModel register(RegisterUserDTO registerDTO) throws DuplicateCreationAttemptException {
         if (userRepository.findByEmail(registerDTO.email).isPresent()) {
@@ -36,17 +25,4 @@ public class AuthorizationService {
 
         return userRepository.save(newUser);
     }
-
-    public String login(LoginDTO loginDTO){
-        var userSaved = userRepository.findByEmail(loginDTO.email);
-
-        if(userSaved.isEmpty())
-            return "error-message";
-
-        var usernamePassword = new UsernamePasswordAuthenticationToken(loginDTO.email, loginDTO.password);
-        var auth = authenticationManager.authenticate(usernamePassword);
-
-        return tokenService.generateToken((User) auth.getPrincipal());
-    }
-
 }
