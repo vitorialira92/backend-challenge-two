5fce4b6b50865bae9a7a06e79b595e5443eb06df
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-20 16:26:30 -0300

    feat: auth controller full implementation

--- src/main/java/mercadoeletronico/Backend/Challenge/Two/controllers/AuthController.java
+++ src/main/java/mercadoeletronico/Backend/Challenge/Two/controllers/AuthController.java
@@ -1,27 +1,68 @@
 package mercadoeletronico.Backend.Challenge.Two.controllers;
 
+import jakarta.validation.Valid;
+import mercadoeletronico.Backend.Challenge.Two.domain.user.UserModel;
+import mercadoeletronico.Backend.Challenge.Two.dtos.LoginResponseDTO;
 import mercadoeletronico.Backend.Challenge.Two.services.AuthorizationService;
-import mercadoeletronico.Backend.Challenge.Two.services.TokenService;
+import mercadoeletronico.Backend.Challenge.Two.dtos.LoginDTO;
+import mercadoeletronico.Backend.Challenge.Two.dtos.RegisterUserDTO;
+import mercadoeletronico.Backend.Challenge.Two.exceptions.DuplicateCreationAttemptException;
+import mercadoeletronico.Backend.Challenge.Two.services.JwtTokenProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 
 @Controller()
 @RequestMapping("/auth")
-
 public class AuthController {
 
     @Autowired
-    private AuthorizationService authorizationService;
+    private AuthorizationService authService;
+
     @Autowired
-    private TokenService tokenService;
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
     @GetMapping("/login")
-    public String getAllSuppliers(Model model){
-        System.out.println("login entrou");
+    public String showLoginForm(Model model) {
+        model.addAttribute("loginDTO", new LoginDTO());
         return "login";
     }
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("registerDTO", new RegisterUserDTO());
+        return "register";
+    }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDTO.email,
+                        loginDTO.password
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String token = tokenProvider.generateToken((UserModel) userDetails);
+
+        String userId = ((UserModel) userDetails).getId();
+
+        return ResponseEntity.ok(new LoginResponseDTO(token, userId, "home-page"));
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterUserDTO registerDTO, Model model) throws DuplicateCreationAttemptException {
+        authService.register(registerDTO);
+        return "login";
+    }
 }
