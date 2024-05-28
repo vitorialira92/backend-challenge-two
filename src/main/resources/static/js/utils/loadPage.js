cf42e2b36f824fd54c780d14b6ce506e2a126d37
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-28 09:51:09 -0300

    fix: pages should not be loaded but redirected

--- src/main/resources/static/js/utils/loadPage.js
+++ src/main/resources/static/js/utils/loadPage.js
@@ -1,25 +1,3 @@
 function loadPage(url) {
-    const token = localStorage.getItem('authToken');
-    $.ajax({
-        url: url,
-        type: 'GET',
-        headers: {
-            'Authorization': token ? 'Bearer ' + token : ''
-        },
-        success: function(response) {
-            $('body').html(response);
-        },
-        error: function(xhr, status, error) {
-            Swal.fire({
-                title: 'Não autorizado.',
-                text: 'Faça login novamente.',
-                icon: 'error',
-                confirmButtonText: 'OK'
-            }).then((result)=>{
-                if (result.value) {
-                    window.location.href = '/login';
-                }
-            });
-        }
-    });
+    window.location.href=url;
 }
