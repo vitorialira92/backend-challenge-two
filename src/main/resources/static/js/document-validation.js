c1d1a7c52af6a6ab442f7d7dee11661b91cf2141
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-28 09:49:28 -0300

    fix: script file change

--- src/main/resources/static/js/document-validation.js
+++ src/main/resources/static/js/document-validation.js
@@ -1,24 +1,3 @@
-document.getElementById('creationForm')
-    .addEventListener('submit', function(event) {
-    var documentValue = $('#documentNumber').val();
-    var type = $('#type').val();
-    var validated = type === 'INDIVIDUAL' ? (validateCPF(documentValue)) : (validateCNPJ(documentValue));
-    if(!validated)
-    {
-        document.getElementById('documentNumber').classList.add('is-invalid');
-        var documentType = type === 'INDIVIDUAL' ? 'CPF' : 'CNPJ';
-        Swal.fire({
-            title: 'Erro!',
-            text: documentType + ' inválido. Por favor, tente novamente.',
-            icon: 'error',
-            confirmButtonText: 'OK'
-        });
-        event.preventDefault();
-    } else {
-        document.getElementById('documentNumber').classList.remove('is-invalid');
-    }
-});
-
 function validateCNPJ(cnpj){
     cnpj = cnpj.replace(/[^\d]+/g,'');
 
@@ -55,38 +34,39 @@ function validateCNPJ(cnpj){
 
 function validateCPF(cpf){
     cpf = cpf.replace(/[^\d]+/g, '');
-
+    console.log("CPF: ", cpf)
     if (cpf === '')
         return false;
-
+    console.log("vazio")
     if (cpf.length !== 11 ||
         /^(\d)\1{10}$/.test(cpf)) return false;
-
+    console.log("tamanho")
     let sum = 0;
     let firstDigit;
 
     for (let i = 1; i <= 9; i++)
         sum += parseInt(cpf.substring(i-1, i)) * (11 - i);
-    firstDigit = sum % 11;
+    firstDigit = 11 - (sum % 11);
 
     if (firstDigit === 10)
         firstDigit = 0;
+    console.log("dígito 1: ", firstDigit)
     if (firstDigit !== parseInt(cpf.substring(9, 10)))
         return false;
-
+    console.log("dígito 1")
     sum = 0;
     let secondDigit;
 
     for (let i = 1; i <= 10; i++)
         sum += parseInt(cpf.substring(i-1, i)) * (12 - i);
-    secondDigit = sum % 11;
+    secondDigit = 11 - sum % 11;
 
     if (secondDigit === 10)
         secondDigit = 0;
 
     if (secondDigit !== parseInt(cpf.substring(10, 11)))
         return false;
-
+    console.log("dígito 2")
     return true;
 }
 
