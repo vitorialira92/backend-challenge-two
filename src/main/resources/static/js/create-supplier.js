a6baca7d74baf593f2adfb6f7d24441b0b6ff9e7
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-05-28 09:49:11 -0300

    fix: script was saving null phone numbers and document validation was supposed to be here

--- src/main/resources/static/js/create-supplier.js
+++ src/main/resources/static/js/create-supplier.js
@@ -2,9 +2,32 @@ $(document).ready(function() {
     $('#creationForm').submit(function(event) {
         event.preventDefault();
 
+        var documentValue = $('#documentNumber').val();
+        var type = $('#type').val();
+
+        var validated = type === 'INDIVIDUAL' ? (validateCPF(documentValue)) : (validateCNPJ(documentValue));
+        console.log("tp ", validateCPF(documentValue))
+        if(!validated)
+        {
+            document.getElementById('documentNumber').classList.add('is-invalid');
+            var documentType = type === 'INDIVIDUAL' ? 'CPF' : 'CNPJ';
+            Swal.fire({
+                title: 'Erro!',
+                text: documentType + ' inválido. Por favor, tente novamente.',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+            return;
+        } else {
+            document.getElementById('documentNumber').classList.remove('is-invalid');
+        }
+
         var phoneNumbers = [];
         $('.phone-input').each(function() {
-            phoneNumbers.push($(this).val());
+            var phoneNumber = $(this).val().trim();
+            if (phoneNumber !== '') {
+                phoneNumbers.push(phoneNumber);
+            }
         });
 
         const token = localStorage.getItem('authToken');
@@ -37,13 +60,22 @@ $(document).ready(function() {
                 console.log('Formulário enviado com sucesso!');
                 loadPage('view-supplier/' + response)
             },
-            error: function(xhr, status, error) {
-                Swal.fire({
-                    title: 'Erro!',
-                    text: 'Houve um problema ao tentar criar o fornecedor! Tente novamente.',
-                    icon: 'error',
-                    confirmButtonText: 'OK'
-                });
+            error: function(xhr) {
+                if(xhr.status === 409){
+                    Swal.fire({
+                        title: 'Erro!',
+                        text: 'Você já possui um fornecedor com esse CPF/CNPJ.',
+                        icon: 'error',
+                        confirmButtonText: 'OK'
+                    });
+                }else{
+                    Swal.fire({
+                        title: 'Erro!',
+                        text: 'Houve um problema ao tentar criar o fornecedor! Tente novamente.',
+                        icon: 'error',
+                        confirmButtonText: 'OK'
+                    });
+                }
             }
         });
     });
